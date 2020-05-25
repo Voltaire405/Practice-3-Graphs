dbb303b05d1fe6dbba79705c3b275500f60f3ee5
Jose Marin <jmarin@sucomunicacion.com>
2020-05-25 10:24:45 -0500

    author: jose.marina description: first commit

---
+++ vendor/js/functions.js
@@ -0,0 +1,13 @@
+/**
+ * descripción: Funciones auxiliares de los módulos.
+ */
+
+ /**
+  * Genera pares ordenados correspondientes a los lados del grafo dirigido y su
+  * correspondiente magnitud.
+  * @param {*} vertexNumber : int Número de vertices que tiene le grafo. 
+  * @returns Array contiene tripletas {origen, destino, longitud}
+  */
+ function generateRandomSides(vertexNumber) {
+     return new Array();
+ }
