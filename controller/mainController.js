181d5a891a3ccae829bb163aaf2195d70eae8362
Jose Marin <jmarin@sucomunicacion.com>
2020-06-02 20:07:11 -0500

    author:jose marin description: integración para una casa

--- controller/mainController.js
+++ controller/mainController.js
@@ -14,9 +14,10 @@ let coordinates = [ //coordenadas de las casas.
     [510, 472],
     [130, 250]
 ];
-let vertex = 0;
+let vertex = 0, vertexOneHouse = 0;
 let sides = new Array();
 let inciMat = new Array();
+let graph = null;
 
 document.getElementById('vertex-form').onsubmit = function () {
     vertex = Number.parseInt(document.getElementById('vertex').value);
@@ -24,6 +25,7 @@ document.getElementById('vertex-form').onsubmit = function () {
     inciMat = getIncidenceMatrix(sides, vertex);    
     hideHouses(vertex);
     renderConnection(sides, coordinates);
+    graph = new Grafo(inciMat);
     document.getElementById('send-vertex').setAttribute('disabled', 'true');
     return false;
 
@@ -32,4 +34,18 @@ document.getElementById('vertex-form').onsubmit = function () {
 document.getElementById('btn-clean').addEventListener("click",function(e){
     window.location.reload(true);
 });
-//let inciMat = getIncidenceMatrix(sides, 9);
+
+document.getElementById('one-house').onsubmit = function () {
+    vertexOneHouse = Number.parseInt(document.getElementById("house").value);
+    document.getElementById("grade").innerHTML = "Grado entrante: " + graph.gradoEntrante(vertexOneHouse) + " Grado saliente: " + graph.gradoSaliente(vertexOneHouse);
+    document.getElementById('send-onehouse').setAttribute('disabled', 'true');
+    return false;
+};
+
+document.getElementById('two-houses').onsubmit = function () {
+    vertex1 = Number.parseInt(document.getElementById("1-house").value);
+    vertex2 = Number.parseInt(document.getElementById("2-house").value);
+    document.getElementById("grade").innerHTML = "Grado entrante: " + graph.gradoEntrante(vertexOneHouse) + " Grado saliente: " + graph.gradoSaliente(vertexOneHouse);
+    document.getElementById('send-onehouse').setAttribute('disabled', 'true');
+    return false;
+};
